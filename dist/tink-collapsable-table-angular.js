836afaf9e9db00a1727d026b4d58bf83fc7ea794
Jasper Van Proeyen <trianglejuice@mac.com>
2015-10-27 11:19:02 +0100

    Build

---
+++ dist/tink-collapsable-table-angular.js
@@ -0,0 +1,29 @@
+'use strict';
+(function(module) {
+  try {
+    module = angular.module('tink.collapsabletable');
+  } catch (e) {
+    module = angular.module('tink.collapsabletable', []);
+  }
+  module.directive('tinkCollapsableTable', [function () {
+    return {
+      restrict: 'EA',
+      scope: {
+        text: '@'
+      },
+      template: '<h1 class="h3 text-center">{{text}}</h1>',
+      replace: true,
+
+      link: function(scope) {
+
+        // Do this on load
+        function initialize() {
+          console.log(scope.text);
+        }
+
+        initialize();
+      }
+    };
+  }]);
+})();
+;
